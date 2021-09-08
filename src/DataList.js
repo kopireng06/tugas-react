172671532dbf59ef034cb2f46d60091695d06c4d
kopireng06 <naufalghaniachmani@gmail.com>
2021-09-08 09:36:08 +0700

    day 11 ni bos

---
+++ src/DataList.js
@@ -0,0 +1,44 @@
+const DataList = [ 
+    {
+      id: 1,
+      title: "Membuat Komponen",
+      completed: true,
+    },
+    {
+      id: 2,
+      title: "Unit Testing",
+      completed: false,
+    },
+    {
+      id: 3,
+      title: "Setup Development Environment",
+      completed: true,
+    },
+    {
+      id: 4,
+      title: "Deploy ke server",
+      completed: false,
+    },
+    {
+      id: 5,
+      title: "Slicing Design to HTML",
+      completed: true,
+    },
+    {
+      id: 6,
+      title: "Refactor Code",
+      completed: false,
+    },
+    {
+      id: 7,
+      title: "Create API",
+      completed: true,
+    },
+    {
+      id: 8,
+      title: "Test Component",
+      completed: false,
+    }
+]
+
+export default DataList;
